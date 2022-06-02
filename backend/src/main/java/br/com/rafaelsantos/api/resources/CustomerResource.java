a1a874c72451605fe0dac8770e43cede9b90c496
Rafael Santos <rafa.05125@gmail.com>
2022-06-02 14:53:32 -0300

    Bean ModelMapper implemented and dto returns to CustomerResource

--- backend/src/main/java/br/com/rafaelsantos/api/resources/CustomerResource.java
+++ backend/src/main/java/br/com/rafaelsantos/api/resources/CustomerResource.java
@@ -1,7 +1,8 @@
 package br.com.rafaelsantos.api.resources;
 
-import br.com.rafaelsantos.api.domain.Customer;
+import br.com.rafaelsantos.api.domain.dto.CustomerDTO;
 import br.com.rafaelsantos.api.services.CustomerService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping( value = "/customer")
 public class CustomerResource {
+
+    @Autowired
+    private ModelMapper mapper;
     @Autowired
     private CustomerService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Customer> findById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<CustomerDTO> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(mapper.map(service.findById(id), CustomerDTO.class));
     }
 }
